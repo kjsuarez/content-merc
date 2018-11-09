44e50de7b20bb06403ed97f3254b12e28c03a168
kjsuarez <kjsuarez94@gmail.com>
2018-11-09 14:53:08 -0800

    init commit, demonstrates fb login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+declare var FB: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'content-merc';
+
+  ngOnInit(){
+
+    (<any>window).fbAsyncInit = ()=> {
+        FB.init({
+          appId            : '1107956112715636',
+          //autoLogAppEvents : true,
+          xfbml            : false,
+          version          : 'v2.9'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+        let js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  public fbLogin(){
+        // this.router.navigate(['./home']);
+        FB.getLoginStatus((response) => {
+            if (response.status === 'connected') {
+                console.log("x")
+            }
+            else {
+                FB.login((loginResponse)=>{
+                    console.log("y")
+                });
+            }
+        });
+    }
 }
